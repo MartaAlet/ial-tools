5adacd4b2b5462f76bce2845522a382f06357ff7
MartaAlet <72511288+MartaAlet@users.noreply.github.com>
2023-06-14 11:36:18 +0200

    Fix error set page config

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
+st.set_page_config(layout="wide", page_title="Tools for the IAL community")
 from predict_page import show_predict_page
 from explore_page import show_explore_page
 from suggestion_page import show_suggestion_page
 
-st.set_page_config(layout="wide")
 page = st.sidebar.selectbox("Choose a page", ("Topic Comparison", "Quality Comparison", "Views per country", "Suggestion Page"))
 
 if page == "Topic Comparison":
