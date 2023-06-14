7c78318f9a391bd44a8c6834e9b7913c5131c0e6
MartaAlet <72511288+MartaAlet@users.noreply.github.com>
2023-06-14 05:34:36 +0200

    Fixed requirements - 2

---
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from predict_page import show_predict_page
+from explore_page import show_explore_page
+from suggestion_page import show_suggestion_page
+
+
+page = st.sidebar.selectbox("Choose a page", ("Topic Comparison", "Quality Comparison", "Views per country", "Suggestion Page"))
+
+if page == "Topic Comparison":
+    show_predict_page()
+elif page == "Quality Comparison":
+    show_explore_page()
+elif page == "Suggestion Page":
+    show_suggestion_page()
+else:
+    show_explore_page()
---
+++ .ipynb_checkpoints/explore_page-checkpoint.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.cluster import AgglomerativeClustering
+import plotly.express as px
+
+@st.cache
+def load_data(file_name):
+    df = pd.read_csv('../'+file_name)
+    return df
+
+df = load_data('mean_predicted_quality.csv')
+df=df.drop(columns=['Unnamed: 0'])
+df_qualities_features_top100 = load_data('df_qualities_features_top100.csv')
+
+
+def show_explore_page():
+    st.markdown("<h1 style='text-align: center; color: #307473;'>Quality Comparison</h1>", unsafe_allow_html=True)
+    
+    st.write("""## Mean Predicted Quality""")
+    
+    col0, col1 = st.columns(2)
+    col0.dataframe(df)
+    fig = px.bar(df, x="language", y="mean predicted quality", color="language", title="Mean Predicted Quality")
+    col1.plotly_chart(fig)
+    st.write("""## Analysis for the top 100 articles of each IAL""")
+    features = ['length (bytes)', 'media', 'wikilinks', 'categories', 'headings', 'references']
+    for feature in features:
+        fig = px.bar(df_qualities_features_top100[df_qualities_features_top100['feature']==feature], x="language", y="count", color="language", title=feature+" count for each IAL")
+        st.plotly_chart(fig)
