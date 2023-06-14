c65485bfa3d92418dd7a4763abcaad10f8264597
MartaAlet <72511288+MartaAlet@users.noreply.github.com>
2023-06-14 10:57:08 +0200

    Incorrect path

--- explore_page.py
+++ explore_page.py
@@ -7,7 +7,7 @@ import plotly.express as px
 
 @st.cache
 def load_data(file_name):
-    df = pd.read_csv('../'+file_name)
+    df = pd.read_csv(file_name)
     return df
 
 df = load_data('mean_predicted_quality.csv')
