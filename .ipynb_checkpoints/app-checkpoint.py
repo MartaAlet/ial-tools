148893c72eae45c2c3b34177727ec6f245e5f045
MartaAlet <72511288+MartaAlet@users.noreply.github.com>
2023-06-17 19:52:19 +0200

    import main page

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -4,6 +4,8 @@ from predict_page import show_predict_page
 from explore_page import show_explore_page
 from suggestion_page import show_suggestion_page
 from worldviews_page import show_worldviews_page
+from MainPage import show_main_page
+
 page = st.sidebar.selectbox("Choose a page", ("Main Page", "Topic Comparison", "Quality Comparison", "Views per country", "Suggestion Page"))
 
 if page == "Topic Comparison":
